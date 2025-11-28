3a8e9739bedc1dfe80c68676e3dc8c9a5ba3aacd
yemukami <kenji@glow-rabbit.com>
2025-11-28 13:27:46 +0900

    Debug: Add error alert to main.js to diagnose startup failure

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import { initUI } from './js/ui/ui-controller.js';
 
-window.onload = function() {
-    initUI();
+window.onload = async function() {
+    try {
+        await initUI();
+    } catch(e) {
+        alert("Application Init Error:\n" + e.message + "\n" + e.stack);
+        console.error(e);
+    }
 };
