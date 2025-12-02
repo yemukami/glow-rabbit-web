facb8fa8c5f8741aadf01990d3bf131fc145438d
yemukami <kenji@glow-rabbit.com>
2025-12-02 10:29:43 +0900

    chore: bump version to v2.1.0-beta.120 and sync docs

--- js/ui/ui-controller.js
+++ js/ui/ui-controller.js
@@ -36,7 +36,7 @@ const UI_CONSTANTS = {
     FINISH_MARGIN_METERS: 50,
     PRESEND_MARGIN_METERS: 10,
     UPDATE_INTERVAL_MS: 100,
-    APP_VERSION: 'v2.1.0-beta.119'
+    APP_VERSION: 'v2.1.0-beta.120'
 };
 
 function formatDisplayPaceLabel(rawPace) {
